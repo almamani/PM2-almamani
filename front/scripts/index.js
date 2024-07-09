a86a2255434e9a12331d3294dc976eb02c80f392
Andrea Mamani <al.mamani@gmail.com>
2024-07-09 20:38:34 -0300

    Homework 1 - Finalizado

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,1 +1,49 @@
-console.log(tempData);
+const crateCard = (film) => {
+  const { poster, title, year, genre, director, duration, rate } = film;
+  const cardPoster = document.createElement("img");
+  const cardTitle = document.createElement("h3");
+  const cardSubtitle = document.createElement("p");
+  const cardGenre = document.createElement("span");
+  const cardDirector = document.createElement("p");
+  const cardDuration = document.createElement("p");
+  const cardRate = document.createElement("p");
+
+  cardPoster.src = poster;
+  cardTitle.innerHTML = title;
+  cardSubtitle.innerHTML = `${year} | <span class="highlight__sky">${genre}</span> `;
+  cardDirector.innerHTML = `<b>Director:</b> ${director}`;
+  cardDuration.innerHTML = `<b>Duración:</b> ${duration}`;
+  cardRate.innerHTML = `<b>Puntaje:</b> ${rate}`;
+
+  cardPoster.className = "peliculas__card__img";
+  cardTitle.className = "peliculas__card__title";
+  cardSubtitle.className = "peliculas__card__subtitle";
+  cardDirector.className = "peliculas__card__text";
+  cardDuration.className = "peliculas__card__text";
+  cardRate.className = "peliculas__card__text";
+
+  const cardFilm = document.createElement("div");
+  cardFilm.className = "peliculas__card";
+
+  cardFilm.appendChild(cardPoster);
+  cardFilm.appendChild(cardTitle);
+  cardFilm.appendChild(cardSubtitle);
+  cardFilm.appendChild(cardGenre);
+  cardFilm.appendChild(cardDirector);
+  cardFilm.appendChild(cardDuration);
+  cardFilm.appendChild(cardRate);
+
+  const cardContainer = document.getElementById("peliculas__container");
+  cardContainer.innerHTML = "";
+
+  return cardFilm;
+};
+
+const showAllCards = () => {
+  const cardContainer = document.getElementById("peliculas__container");
+  cardContainer.innerHTML = "";
+  const cardsFilms = tempData.map((film) => crateCard(film));
+  cardsFilms.forEach((film) => cardContainer.appendChild(film));
+};
+
+showAllCards();
--- front/scripts/tempData.js
+++ front/scripts/tempData.js
@@ -29,4 +29,34 @@ const tempData = [
     poster:
       "https://m.media-amazon.com/images/M/MV5BN2EyZjM3NzUtNWUzMi00MTgxLWI0NTctMzY4M2VlOTdjZWRiXkEyXkFqcGdeQXVyNDUzOTQ5MjY@._V1_SX300.jpg",
   },
+  {
+    title: "Titanic",
+    year: 1997,
+    director: "James Cameron",
+    duration: "3h 15min",
+    genre: ["Drama", "Catastrophe"],
+    rate: 7.5,
+    poster:
+      "https://cdn.posteritati.com/posters/000/000/049/967/titanic-md-web.jpg",
+  },
+  {
+    title: "Aladdín",
+    year: 2019,
+    director: "Guy Ritchie",
+    duration: "2h 08min",
+    genre: ["Fancy", "Musical", "Aventure"],
+    rate: 8.5,
+    poster:
+      "https://www.originalfilmart.com/cdn/shop/products/aladdin_1992_original_film_art_a_3fdc1d40-687d-452a-8f49-3da921664304_600x.jpg?v=1640133368",
+  },
+  {
+    title: "Love Actually",
+    year: 2003,
+    director: "Richard Curtis",
+    duration: "2h 16min",
+    genre: ["Romantic Comedy"],
+    rate: 9.5,
+    poster:
+      "https://www.movieposters.com/cdn/shop/products/b24f145509265ebe6e06a6d9d5dffab7_697d3693-0ca6-49dd-b3d1-d56fbf893629_500x.jpg?v=1573593648",
+  },
 ];
