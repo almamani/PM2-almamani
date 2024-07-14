bcb6942078c3bbd05deb876d741a4ca4b67ca44b
Andrea Mamani <al.mamani@gmail.com>
2024-07-14 11:27:26 -0300

    Homework 2 - Finalizado

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -6,21 +6,18 @@ const crateCard = (film) => {
   const cardGenre = document.createElement("span");
   const cardDirector = document.createElement("p");
   const cardDuration = document.createElement("p");
-  const cardRate = document.createElement("p");
 
   cardPoster.src = poster;
   cardTitle.innerHTML = title;
-  cardSubtitle.innerHTML = `${year} | <span class="highlight__sky">${genre}</span> `;
+  cardSubtitle.innerHTML = `<span class="highlight__sky">${genre}</span> `;
   cardDirector.innerHTML = `<b>Director:</b> ${director}`;
   cardDuration.innerHTML = `<b>Duración:</b> ${duration}`;
-  cardRate.innerHTML = `<b>Puntaje:</b> ${rate}`;
 
   cardPoster.className = "peliculas__card__img";
   cardTitle.className = "peliculas__card__title";
   cardSubtitle.className = "peliculas__card__subtitle";
   cardDirector.className = "peliculas__card__text";
   cardDuration.className = "peliculas__card__text";
-  cardRate.className = "peliculas__card__text";
 
   const cardFilm = document.createElement("div");
   cardFilm.className = "peliculas__card";
@@ -31,19 +28,23 @@ const crateCard = (film) => {
   cardFilm.appendChild(cardGenre);
   cardFilm.appendChild(cardDirector);
   cardFilm.appendChild(cardDuration);
-  cardFilm.appendChild(cardRate);
-
-  const cardContainer = document.getElementById("peliculas__container");
-  cardContainer.innerHTML = "";
 
   return cardFilm;
 };
 
-const showAllCards = () => {
+const showAllCards = (data) => {
   const cardContainer = document.getElementById("peliculas__container");
   cardContainer.innerHTML = "";
-  const cardsFilms = tempData.map((film) => crateCard(film));
+  const cardsFilms = data.map((film) => crateCard(film));
   cardsFilms.forEach((film) => cardContainer.appendChild(film));
 };
 
-showAllCards();
+$.get("https://students-api.up.railway.app/movies", (data) => {
+  showAllCards(data);
+}).fail((jqXHR, textStatus, errorThrown) => {
+  console.log(`Error: ${jqXHR.status}`);
+  const error = document.createElement("h2");
+  error.innerHTML = "Listado de Peliculas Inaccesible Momentáneamente";
+  const errorContainer = document.getElementById("container__error");
+  errorContainer.appendChild(error);
+});
--- front/scripts/tempData.js
+++ front/scripts/tempData.js
@@ -1,35 +1,5 @@
 const tempData = [
   {
-    title: "Guardians of the Galaxy Vol. 2",
-    year: 2017,
-    director: "James Gunn",
-    duration: "2h 16min",
-    genre: ["Action", "Adventure", "Comedy"],
-    rate: 7.7,
-    poster:
-      "https://m.media-amazon.com/images/M/MV5BNjM0NTc0NzItM2FlYS00YzEwLWE0YmUtNTA2ZWIzODc2OTgxXkEyXkFqcGdeQXVyNTgwNzIyNzg@._V1_SX300.jpg",
-  },
-  {
-    title: "Star Wars: Episode IV - A New Hope",
-    year: 1977,
-    director: "George Lucas",
-    duration: "2h 1min",
-    genre: ["Action", "Adventure", "Fantasy", "Sci-Fi"],
-    rate: 8.7,
-    poster:
-      "https://m.media-amazon.com/images/M/MV5BOTA5NjhiOTAtZWM0ZC00MWNhLThiMzEtZDFkOTk2OTU1ZDJkXkEyXkFqcGdeQXVyMTA4NDI1NTQx._V1_SX300.jpg",
-  },
-  {
-    title: "The Lord of the Rings: The Fellowship of the Ring",
-    year: 2001,
-    director: "Peter Jackson",
-    duration: "2h 58min",
-    genre: ["Action", "Adventure", "Drama", "Fantasy"],
-    rate: 8.8,
-    poster:
-      "https://m.media-amazon.com/images/M/MV5BN2EyZjM3NzUtNWUzMi00MTgxLWI0NTctMzY4M2VlOTdjZWRiXkEyXkFqcGdeQXVyNDUzOTQ5MjY@._V1_SX300.jpg",
-  },
-  {
     title: "Titanic",
     year: 1997,
     director: "James Cameron",
@@ -58,5 +28,35 @@ const tempData = [
     rate: 9.5,
     poster:
       "https://www.movieposters.com/cdn/shop/products/b24f145509265ebe6e06a6d9d5dffab7_697d3693-0ca6-49dd-b3d1-d56fbf893629_500x.jpg?v=1573593648",
+  },
+  {
+    title: "A Beautiful Mind",
+    year: 2001,
+    director: "Ron Howard",
+    duration: "2h 15min",
+    genre: ["Drama"],
+    rate: 10,
+    poster:
+      "https://i.pinimg.com/originals/90/a0/3f/90a03f2219e74c8d076545c710eecc49.png",
+  },
+  {
+    title: "Bloodsport",
+    year: 2003,
+    director: "Newt Arnold",
+    duration: "1h 32min",
+    genre: ["Action", "Martial Arts"],
+    rate: 9.5,
+    poster:
+      "https://image.tmdb.org/t/p/original/bgOrs8YLOstkoxHfbGN9OkVTFWx.jpg",
+  },
+  {
+    title: "El Hijo de la Novia",
+    year: 2001,
+    director: "Juan José Campanella",
+    duration: "2h 03min",
+    genre: ["Romantic Drama"],
+    rate: 10,
+    poster:
+      "https://pics.filmaffinity.com/El_hijo_de_la_novia-971118410-large.jpg",
   },
 ];
