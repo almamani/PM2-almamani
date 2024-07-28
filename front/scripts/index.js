5aa1415192b242b973853d12b595ba25a3809a6b
Andrea Mamani <al.mamani@gmail.com>
2024-07-28 14:16:39 -0300

    Homework 6 - Finalizado

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,13 +1,19 @@
 const tempData = require("./tempData");
 const showAllCards = require("./showAllCards");
+const axios = require("axios");
 
-$.get("https://students-api.up.railway.app/movies")
-  .done((results) => {
-    showAllCards(results);
-  })
-  .fail(() => {
+const fetchMovies = async () => {
+  try {
+    const results = await axios.get(
+      "https://students-api.up.railway.app/movies"
+    );
+    showAllCards(results.data);
+  } catch (error) {
     alert(
       "No podemos acceder al listado de películas actualizado, por el momento generaremos uno provisorio"
     );
     showAllCards(tempData);
-  });
+  }
+};
+
+fetchMovies();
