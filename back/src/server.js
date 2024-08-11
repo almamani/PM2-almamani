14c070a3b94ee2e8540e804ceafe9cb8916c9277
Andrea Mamani <al.mamani@gmail.com>
2024-08-11 19:45:49 -0300

    Correcciones varias Front Back

--- back/src/server.js
+++ back/src/server.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const router = require("./routes");
-const cors = require('cors');
+const morgan = require("morgan");
+const cors = require("cors");
 
 const app = express();
 
+app.use(express.json());
+app.use(morgan("dev"));
 app.use(cors());
+
 app.use(router);
 
 module.exports = app;
